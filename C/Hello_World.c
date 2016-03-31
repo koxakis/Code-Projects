a5ba86eb517bfde9bf3c688cf9e5ef4196e93f61
Nikolas Koxenoglou <koxakin@gmail.com>
2016-03-31 21:58:28 +0300

    Make Hello_World.c more interesting

--- C/Hello_World.c
+++ C/Hello_World.c
@@ -2,8 +2,8 @@
 
 int main( int argc, char **argv){
 
-    //Outputs "Hello World" to the screen
-    printf("Hello world \n");
+    //Outputs "Hello World I am a Simple C program" to the screen
+    printf("Hello world \n I am a Simple C program");
 
     return(0);
 }
