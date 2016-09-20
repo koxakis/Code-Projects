3e557d680bd3609e83f936506a80805dc66a24ce
Nikolas Koxenoglou <koxakin@gmail.com>
2016-09-20 22:17:47 +0300

    Updated with dynamic memory managment

--- C/SOJ/Challenge_1-Dominos/Dominos.c
+++ C/SOJ/Challenge_1-Dominos/Dominos.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main(int argc, char const *argv[]) {
 
 	int t, c, k, w, i=0;
-	char res[100];
+	char** res;
 
 	scanf( "%d", &t);
 	//Check to see if tries are within the given range
@@ -11,26 +12,30 @@ int main(int argc, char const *argv[]) {
 		return 0;
 	}
 
+	res = ( char**)malloc( sizeof(char**) * t);
+
 	do {
 		scanf("%d %d %d", &c, &k, &w);
-		if ( ( c > 100 || c < 1) ||
-			( k > 100 || k < 1) ||
-			( w > 100 || w < 1)) {
-
-				return 0 ;
-			}
+		//Check to see if variables are within the given range
+		if ( ( c > 100 || c < 1) || ( k > 100 || k < 1) || ( w > 100 || w < 1)) {
+			return 0 ;
+		}
 
 		if ( k >= ( c * w )){
-			//printf( "Yes \n");
-			res[i] = 'Y';
+			res[i] = "Yes";
 		} else{
-			//printf( "No \n");
-			res[i] = 'N';
+			res[i] = "No";
 		}
 		i++;
 	} while(i < t);
 
-	printf("%s\n", res);
+	i = 0;
+	do {
+		printf("%s\n", res[i]);
+		i++;
+	} while(i < t);
+
+	free(res);
 
 	return 0;
 }
