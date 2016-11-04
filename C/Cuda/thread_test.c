00bfa6ce0f4cd66965f4912205e833202e978305
Nikolas Koxenoglou <koxakin@gmail.com>
2016-11-04 23:16:35 +0200

    Implemented Host machine code

--- C/Cuda/thread_test.c
+++ C/Cuda/thread_test.c
@@ -1,15 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//////////////////////////////////////////////////////////////////////////////////////
+//Host Code
+//////////////////////////////////////////////////////////////////////////////////////
+void convolutionHostRow(double **input, double **output, int dim_x, int dim_y) {
+
+	for (int x= 0; x < dim_x; x++) {
+		for (int y=0 ; y < dim_y; y++) {
+
+			output[x][y] = -input[x][y];
+		}
+	}
+}
+void convolutionHostColumn(double **input, double **output, int dim_x, int dim_y) {
+
+	for (int x= 0; x < dim_x; x++) {
+		for (int y=0 ; y < dim_y; y++) {
+
+			output[x][y] = input[y][x];
+		}
+	}
+}
+
 int main(int argc, char const *argv[]) {
 
 	double
 	**h_matrix ,
+	**h_buffer ,
 	**h_OutputCPU;
 
-	int dim_x, dim_y;
+	int dim_x, dim_y, max_withd = 3;
 
-	printf("Intert a x and y dims");
+	printf("Intert a x and y dims ");
 	scanf("%d %d", &dim_x, &dim_y);
 
 	h_matrix = (double**)malloc(sizeof(double*)*dim_y);
@@ -21,12 +44,45 @@ int main(int argc, char const *argv[]) {
 		h_matrix[i] = (double*)realloc(h_matrix[i], sizeof(double) * dim_y);
 		if (h_matrix[i] == NULL){
 			fprintf(stderr, "Error in Host Matrix allocation\n" );
+			exit(EXIT_FAILURE);
+		}
+		for ( int j = 0; j < dim_y; j++) {
+			h_matrix[i][j] = j + (i * dim_x);
 		}
 	}
 
-	for (int j = 0; j < dim_y; j++) {
-		for ( int i = 0; i < dim_x; i++){
-			printf(" %g", h_matrix[i][j]);
+	h_buffer = (double**)malloc(sizeof(double*)*dim_y);
+	if ( h_buffer == NULL) {
+		fprintf(stderr, "Error in Host Matrix allocation\n" );
+		exit(EXIT_FAILURE);
+	}
+	for ( int i = 0; i < dim_x; i++){
+		h_buffer[i] = (double*)realloc(h_buffer[i], sizeof(double) * dim_y);
+		if ( h_buffer[i] == NULL) {
+			fprintf(stderr, "Error in Host Matrix allocation\n" );
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	h_OutputCPU = (double**)malloc(sizeof(double*)*dim_y);
+	if ( h_buffer == NULL) {
+		fprintf(stderr, "Error in Host Matrix allocation\n" );
+		exit(EXIT_FAILURE);
+	}
+	for ( int i = 0; i < dim_x; i++){
+		h_OutputCPU[i] = (double*)realloc(h_OutputCPU[i], sizeof(double) * dim_y);
+		if ( h_OutputCPU[i] == NULL) {
+			fprintf(stderr, "Error in Host Matrix allocation\n" );
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	convolutionHostRow(h_matrix, h_buffer, dim_x, dim_y);
+	convolutionHostColumn(h_buffer, h_OutputCPU, dim_x, dim_y);
+
+	for ( int i = 0; i < dim_x; i++){
+		for (int j = 0; j < dim_y; j++) {
+			printf(" %*g", max_withd, h_OutputCPU[i][j]);
 		}
 		printf("\n");
 	}
